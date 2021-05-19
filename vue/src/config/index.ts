25b942374abb6f73d603d45bc4733a48bd3875ce
ddosdor <ddosdor@gmail.com>
2021-05-19 15:27:44 +0200

    (vue:none): Remove config file

--- vue/src/config/index.ts
+++
@@ -1,8 +0,0 @@
-import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
-
-const API_CONFIG: AxiosRequestConfig = {
-  baseURL: process.env.VUE_APP_API_URL,
-  timeout: 5000,
-};
-
-export const API: AxiosInstance = axios.create(API_CONFIG);
